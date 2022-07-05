160ee53268320ed0de10ed19d8dd2e40061bd58e
Alexis Javier Levano Saire <levanosaire@gmail.com>
2022-07-05 16:04:07 -0500

    first commit

--- src/Hero.js
+++ src/Hero.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
-import { useGlobalContext } from './context'
+import React from 'react';
+import phoneImg from './images/phone.svg';
+import { useGlobalContext } from './context';
+
 const Hero = () => {
-  const {closeSubmenu} = useGlobalContext()
-
-  return( 
+  const { closeSubmenu } = useGlobalContext();
+  return (
     <section className='hero' onMouseOver={closeSubmenu}>
-    <div className='hero-center'>
-      <article className='hero-info'>
-        <h1> Infraestructura por Internet</h1>
-        <p>Millones de compañias de todos los tamaños desde Startups hasta las mejores 500 según Fortune usan Stripe software y sus Apis para aceptar pagos, enviar dinero y manejar sus negocios en linea</p>
-        <button className='btn'>Empieza Ahora</button>
-      </article>
-      <article className='hero-images'>
-        <img src={phoneImg} className='phone-img' alt='phone'/> 
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-      </article>
-
-    </div>
-    
-
-
-  </section>
-)}
-
-export default Hero
+export default Hero;
